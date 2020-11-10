8eefe5b93f17728ddf3e0cfb55ce8051e68fef9c
Cristian <ccamilo2303@gmail.com>
2020-11-09 20:43:38 -0500

    Primmer commit

---
+++ integracionContinuaService/src/main/java/com/integracion/continua/ContinuaApplication.java
@@ -0,0 +1,13 @@
+package com.integracion.continua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContinuaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContinuaApplication.class, args);
+	}
+
+}
---
+++ integracionContinuaService/src/test/java/com/integracion/continua/ContinuaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integracion.continua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContinuaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
